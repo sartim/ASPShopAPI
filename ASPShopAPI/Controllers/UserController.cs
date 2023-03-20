3ce7966d272cc9576c301726cc6cdad05fbcd59d
Tim <sarrtim@gmail.com>
2023-03-20 17:07:11 +0300

    Add usser controller overriding posst method

--- ASPShopAPI/Controllers/UserController.cs
+++ ASPShopAPI/Controllers/UserController.cs
@@ -1,96 +1,27 @@
 ﻿using System;
-using ASPShopAPI.Data;
-using ASPShopAPI.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ASPShopAPI.Models;
+using ASPShopAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPShopAPI.Controllers
 {
-    [Authorize]
-    [Route("api/v1")]
-	[ApiController]
-	public class UserController : ControllerBase
-	{
-		private readonly UserDbContext _context;
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : BaseController<User>
+    {
 
-		public UserController(UserDbContext context)
-		{
-			_context = context;
-		}
-        // Create
-        [HttpPost("users")]
-        public JsonResult Create(User user)
+        public UserController(ShopDbContext dbContext) : base(dbContext)
         {
-            // hash the password before saving the user
+        }
+
+        // POST: api/user
+        [HttpPost]
+        public override async Task<ActionResult<User>> Post(User user)
+        {
             user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
-
-            _context.Users.Add(user);
-
-            _context.SaveChanges();
-
-            return new JsonResult(Ok(user));
+            return await base.Post(user);
         }
 
-
-        // Edit
-        [HttpPut("users/{id}")]
-        public JsonResult Edit(User user)
-        {
-            var userInDb = _context.Users.Find(user.Id);
-
-            if (userInDb == null)
-                return new JsonResult(NotFound());
-
-            // update the user's password if it has been changed
-            if (user.Password != userInDb.Password)
-            {
-                userInDb.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
-            }
-
-            userInDb.FirstName = user.FirstName;
-            userInDb.Email = user.Email;
-
-            _context.SaveChanges();
-
-            return new JsonResult(Ok(user));
-        }
-
-
-        // Get
-        [HttpGet("users/{id}")]
-		public JsonResult Get(Guid id)
-		{
-			var result = _context.Users.Find(id);
-
-			if (result == null)
-				return new JsonResult(NotFound());
-
-			return new JsonResult(Ok(result));
-		}
-
-		// Delete
-		[HttpDelete("users/{id}")]
-		public JsonResult Delete(Guid id)
-		{
-			var result = _context.Users.Find(id);
-
-			if (result == null)
-				return new JsonResult(NotFound());
-
-			_context.Users.Remove(result);
-			_context.SaveChanges();
-
-			return new JsonResult(NoContent());
-		}
-
-		// Get all
-		[HttpGet("users")]
-		public JsonResult GetAll()
-		{
-			var result = _context.Users.ToList();
-            return new JsonResult(Ok(result));
-        }
-	}
+    }
 }
-
