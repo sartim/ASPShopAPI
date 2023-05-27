04adb4c93873ebb1c4142555094f80559a0b55ce
Tim <sarrtim@gmail.com>
2023-05-27 20:45:29 +0300

    Add permission controller

---
+++ ASPShopAPI/Controllers/PermissionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ASPShopAPI.Models;
+using ASPShopAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPShopAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PermissionController : BaseController<User>
+    {
+        public PermissionController(ShopDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
