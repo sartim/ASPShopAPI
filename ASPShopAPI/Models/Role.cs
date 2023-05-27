9e34996b8cdc0555bce84b7396e25b0b6f46221d
Tim <sarrtim@gmail.com>
2023-05-27 20:44:12 +0300

    Add role model

---
+++ ASPShopAPI/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ASPShopAPI.Models
+{
+	public class Role
+	{
+        public Guid   Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool   IsDeleted { get; set; }
+        public bool   CreatedAt { get; set; }
+        public bool   UpdatedAt { get; set; }
+        public bool   DeletedAt { get; set; }
+    }
+}
+
