e259aeb90080df0e0462b7b9863cd84e0dd72289
Tim <sarrtim@gmail.com>
2023-05-27 20:44:56 +0300

    Add role controller

---
+++ ASPShopAPI/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ASPShopAPI.Models;
+using ASPShopAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPShopAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class RoleController : BaseController<User>
+    {
+        public RoleController(ShopDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
