5403413670daa55991129503f4293a0a8e7f382e
Tim <sarrtim@gmail.com>
2023-06-19 21:32:03 +0300

    Add role permission model

---
+++ ASPShopAPI/Models/RolePermission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPShopAPI.Models
+{
+    public class RolePermissions
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Role")]
+        public Guid RoleId { get; set; }
+
+        [ForeignKey("Permission")]
+        public Guid PermissionId { get; set; }
+
+        public Role Role { get; set; }
+        public Permission Permission { get; set; }
+    }
+}
+
