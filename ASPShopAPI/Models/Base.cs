664127563321c9dd98aa0a507860ddce8e0cb3d3
Tim <sarrtim@gmail.com>
2024-06-05 11:32:15 +0300

    Update Base model

--- ASPShopAPI/Models/Base.cs
+++ ASPShopAPI/Models/Base.cs
@@ -4,11 +4,11 @@ namespace ASPShopAPI.Models
 {
     	public class Base
 	{
-        public Guid     Id { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
-        public DateTime DeletedAt { get; set; }
+        public Guid      Id { get; set; }
+        public bool      IsDeleted { get; set; } = false;
+        public DateTime  CreatedAt { get; set; } = DateTime.Now;
+        public DateTime  UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime? DeletedAt { get; set; }
     }
 }
 
