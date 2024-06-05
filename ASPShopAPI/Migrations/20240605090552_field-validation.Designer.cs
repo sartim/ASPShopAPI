9304b08dd1307320ac7a82914a1464e906524641
Tim <sarrtim@gmail.com>
2024-06-05 12:06:34 +0300

    Add field validation migration

--- ASPShopAPI/Migrations/20240605090552_field-validation.Designer.cs
+++ ASPShopAPI/Migrations/20240605090552_field-validation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ASPShopAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ASPShopAPI.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    partial class ShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605090552_field-validation")]
+    partial class fieldvalidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
